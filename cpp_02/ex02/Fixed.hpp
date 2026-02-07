c8bc9c7e04630199c6f1b2ea07c60f7e8c10404f
edlucca <lucca.ed@gmail.com>
2026-02-07 22:34:56 +0200

    cpp_02-ex02

--- cpp_02/ex02/Fixed.hpp
+++ cpp_02/ex02/Fixed.hpp
@@ -15,25 +15,39 @@ class Fixed {
 	public:
 		Fixed();
 		Fixed(const int number);
-		Fixed(const float almostreal);
+		Fixed(const float number);
 		Fixed(const Fixed& other);
 		Fixed& operator = (const Fixed& other);
-		Fixed& operator > (const Fixed& other);
-		Fixed& operator < (const Fixed& other);
-		Fixed& operator >= (const Fixed& other);
-		Fixed& operator <= (const Fixed& other);
-		Fixed& operator == (const Fixed& other);
-		Fixed& operator != (const Fixed& other);
-		Fixed& operator + (const Fixed& other);
-		Fixed& operator - (const Fixed& other);
-		Fixed& operator * (const Fixed& other);
-		Fixed& operator / (const Fixed& other);
 		~Fixed();
 
+		// Member function
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
 		float	toFloat(void) const;
 		int		toInt(void) const;
+		// Comparison operators overload
+		bool	operator > (const Fixed& other) const;
+		bool	operator < (const Fixed& other) const;
+		bool	operator >= (const Fixed& other) const;
+		bool	operator <= (const Fixed& other) const;
+		bool	operator == (const Fixed& other) const;
+		bool	operator != (const Fixed& other) const;
+		// Arithmetic operators overload
+		Fixed& operator + (const Fixed& other) const;
+		Fixed& operator - (const Fixed& other) const;
+		Fixed& operator * (const Fixed& other) const;
+		Fixed& operator / (const Fixed& other) const;
+		// Increment/decremente operators overload
+		Fixed& operator ++ (void);
+		Fixed operator ++ (int);
+		Fixed& operator -- (void);
+		Fixed operator -- (int);
+		// Static member function
+		static Fixed& min(Fixed &a, Fixed &b);
+		static Fixed& max(Fixed &a, Fixed &b);
+		static const Fixed& min(const Fixed &a, const Fixed &b);
+		static const Fixed& max(const Fixed &a, const Fixed &b);
+
 };
 
 std::ostream& operator<<(std::ostream& out, const Fixed& value);
