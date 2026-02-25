b42811d04d11663f7444af4bdd8eda9e215fdc32
Eduardo Lucca <edlucca@student.hive.fi>
2026-02-25 17:13:18 +0200

    cpp05-ex00 started

--- cpp_05/ex00/include/Bureaucrat.hpp
+++ cpp_05/ex00/include/Bureaucrat.hpp
@@ -2,6 +2,7 @@
 #define BUREAUCRAT_HPP
 
 #include <iostream>
+#include <exception>
 
 class Bureaucrat {
 	private:
@@ -14,10 +15,22 @@ class Bureaucrat {
 		Bureaucrat &operator=(const Bureaucrat &other);
 		~Bureaucrat(void);
 
+		Bureaucrat(std::string name, int grade);
 		const std::string	getName() const;
 		int		getGrade() const;
-		void	incrementGrade() const;
-		void	decrementGrade() const;
+		void	incrementGrade();
+		void	decrementGrade();
+
+		class GradeTooHighException : public std::exception {
+			public:
+				const	char*	what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception {
+			public:
+				const	char*	what() const throw();
+		};
 };
 
+std::ostream &operator<<(std::ostream &outstream, Bureaucrat &corporate);
 #endif // BUREAUCRAT_HPP
