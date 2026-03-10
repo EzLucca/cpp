26f05b05a3b2376a08b1af9834f06e80553745f6
Eduardo Lucca <edlucca@student.hive.fi>
2026-03-10 17:04:56 +0200

    cpp06-ex00

--- cpp_06/ex00/include/ScalarConverter.hpp
+++ cpp_06/ex00/include/ScalarConverter.hpp
@@ -1,14 +1,19 @@
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
 class ScalarConverter {
 	private:
 
 	public:
-		ScalarConverter(void);
-		ScalarConverter(const ScalarConverter &other);
-		ScalarConverter &operator=(const ScalarConverter &other);
+		ScalarConverter(void) = delete;
+		ScalarConverter(const ScalarConverter &other) = delete;
+		ScalarConverter &operator=(const ScalarConverter &other) = delete;
 		~ScalarConverter(void);
+		static void convert(const std::string& literal);
 };
 
 #endif // SCALARCONVERTER_HPP
--- cpp_06/ex00/src/ScalarConverter.cpp
+++ cpp_06/ex00/src/ScalarConverter.cpp
@@ -1,34 +1,58 @@
 #include "ScalarConverter.hpp"
 
 /**
- * @brief Default constructor.
- *
- * Initializes the ScalarConverter with a default type.
- */
-ScalarConverter::ScalarConverter(void) {}
-
-/**
- * @brief Copy constructor.
- *
- * Creates a new ScalarConverter by copying another one.
- *
- * @param other The ScalarConverter object to copy from.
- */
-ScalarConverter::ScalarConverter(const ScalarConverter &other) {}
-
-/**
- * @brief Copy assignment operator.
- *
- * Assigns the values of another ScalarConverter to this instance.
- *
- * @param other The ScalarConverter object to assign from.
- * @return Reference to the assigned ScalarConverter.
- */
-ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other) {}
-
-/**
  * @brief Destructor.
  *
  * Called when the ScalarConverter object is destroyed.
  */
 ScalarConverter::~ScalarConverter(void) {}
+
+static bool isChar(const std::string& s) {
+	return s.length() == 1 && !isdigit(s[0]);
+}
+
+static bool isPseudo(const std::string& s) {
+	return ( s == "nan" || s == "nanf" ||
+			s == "+inf" || s == "-inf" ||
+			s == "+inff" || s == "-inff"); 
+}
+
+void ScalarConverter::convert(const std::string& literal){
+
+	double value;
+
+	if (isPseudo(literal))
+		value = std::strtod(literal.c_str(), NULL);
+	else if(isChar(literal))
+		value = static_cast<double>(literal[0]);
+	else
+		value = std::strtod(literal.c_str(), NULL);
+
+	// char
+	std::cout << "char: ";
+	if (std::isnan(value) || value < 0 || value > 127)
+		std::cout << "impossible\n";
+	else if (!isprint(static_cast<char>(value)))
+		std::cout << "Non displayable\n";
+	else
+		std::cout << "'" << static_cast<char>(value) << "'\n";
+
+	// int
+	std::cout << "int: ";
+	if (std::isnan(value) ||
+			value > std::numeric_limits<int>::max() ||
+			value < std::numeric_limits<int>::min())
+		std::cout << "impossible\n";
+	else
+		std::cout << static_cast<int>(value) << "\n";
+
+	// float
+	std::cout << "float: ";
+	std::cout << std::fixed << std::setprecision(1)
+		<< static_cast<float>(value) << "f\n";
+
+	// double
+	std::cout << "double: ";
+	std::cout << std::fixed << std::setprecision(1)
+		<< static_cast<double>(value) << "\n";
+}
