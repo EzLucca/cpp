405a09565dfed01214cd9a62982e4f53c345eccc
Eduardo Lucca <edlucca@student.hive.fi>
2026-03-25 17:24:31 +0200

    edited cpp06-ex00 main

--- cpp_06/ex00/src/main.cpp
+++ cpp_06/ex00/src/main.cpp
@@ -1,9 +1,25 @@
 #include "ScalarConverter.hpp"
 
-int main(int argc, char **argv)
-{
-    if (argc != 2)
-        return 1;
+// int main(int argc, char **argv)
+// {
+//     if (argc != 2)
+//         return 1;
+//
+//     ScalarConverter::convert(argv[1]);
+// }
 
-    ScalarConverter::convert(argv[1]);
+int main()
+{
+    ScalarConverter::convert("2147483647");
+	std::cout << "---\n";
+    ScalarConverter::convert("-2147483648");
+	std::cout << "---\n";
+    ScalarConverter::convert("2147483648");
+	std::cout << "---\n";
+    ScalarConverter::convert("2147jfksdjf483648");
+	std::cout << "---\n";
+    ScalarConverter::convert("2147jfksdjf483648");
+	std::cout << "---\n";
+    ScalarConverter::convert("214747598234758972304570274358728074507408957230975483648");
+	std::cout << "---\n";
 }
