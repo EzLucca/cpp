90773b9e6bf7b349aa3724d13525666ff9228b07
Eduardo Lucca <edlucca@student.hive.fi>
2026-07-08 16:40:19 +0300

    ex02 start

--- cpp_09/ex02/src/main.cpp
+++ cpp_09/ex02/src/main.cpp
@@ -1,1 +1,40 @@
 #include "PmergeMe.hpp"
+#include <string>
+#include <charconv>
+#include <string_view>
+
+bool isPositiveInteger(const char* str)
+{
+    if (str == nullptr || *str == '\0')
+        return false;
+
+    std::string_view sv(str);
+
+    int value{};
+    auto [ptr, ec] = std::from_chars(
+        sv.data(),
+        sv.data() + sv.size(),
+        value
+    );
+
+    return ec == std::errc{} &&
+           ptr == sv.data() + sv.size() &&
+           value > 0;
+}
+
+int main (int argc, char **argv)
+{
+    // validate argvs to create unsorted list
+    if (argc < 2)
+        std::cerr << "./program args[...]" << std::endl;
+    while (*argv)
+    {
+        //validate argv
+        if(!isPositiveInteger(*argv))
+            std::cout << "Wrong: "  << *argv << std::endl;
+        else
+            std::cout << *argv << std::endl;
+        argv++;
+    }
+    return 0;
+}
