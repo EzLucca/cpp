ceff9382a0c417c3b8cd4b833e93241eeb1f169b
Eduardo Lucca <edlucca@student.hive.fi>
2026-07-13 16:24:48 +0300

    PmergeMe.hpp

--- cpp_09/ex02/src/PmergeMe.cpp
+++ cpp_09/ex02/src/PmergeMe.cpp
@@ -1,1 +1,13 @@
 #include "PmergeMe.hpp"
+
+#include <deque>
+#include <vector>
+#include <chrono>
+
+class PmergeMe
+{
+    private:
+        std::deque<int> _deque;
+        std::vector<int> _vector;
+    public:
+};
--- cpp_09/ex02/src/main.cpp
+++ cpp_09/ex02/src/main.cpp
@@ -27,14 +27,16 @@ int main (int argc, char **argv)
     // validate argvs to create unsorted list
     if (argc < 2)
         std::cerr << "./program args[...]" << std::endl;
-    while (*argv)
-    {
-        //validate argv
-        if(!isPositiveInteger(*argv))
-            std::cout << "Wrong: "  << *argv << std::endl;
-        else
-            std::cout << *argv << std::endl;
-        argv++;
-    }
+    for (int i = 1; i < argc; i++)
+        {
+            if (!isPositiveInteger(argv[i]))
+            {
+                std::cerr << "Wrong: " << argv[i] << std::endl;
+                exit (1);
+            }
+            else
+                std::cout << argv[i] << " ";
+        }
+    std::cout << std::endl;
     return 0;
 }
